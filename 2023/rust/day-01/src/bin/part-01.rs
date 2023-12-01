d70faf49bf91823463ddd557f488746c4d5f0505
Roberto Mora <romogo17@gmail.com>
2023-12-01 11:20:22 -0600

    adds day-01-part-01

---
+++ 2023/rust/day-01/src/bin/part-01.rs
@@ -0,0 +1,39 @@
+fn main() {
+    let input = include_str!("../../inputs/part-01-input-01.txt");
+    let output = process(input);
+    println!("Output is {output}");
+}
+
+fn process(input: &str) -> u32 {
+    input.split('\n').map(|line| process_line(line)).sum()
+}
+
+fn process_line(line: &str) -> u32 {
+    let digits: Vec<u32> = line.chars().filter_map(|d| d.to_digit(10)).collect();
+    let first = digits.first().unwrap();
+    let last = digits.last().unwrap();
+
+    [first, last].iter().fold(0, |acc, &x| acc * 10 + x)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_01_example() {
+        let output = process(
+            "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet",
+        );
+        assert_eq!(output, 142);
+    }
+
+    #[test]
+    fn part_01_input_01() {
+        let output = process(include_str!("../../inputs/part-01-input-01.txt"));
+        assert_eq!(output, 53651);
+    }
+}
