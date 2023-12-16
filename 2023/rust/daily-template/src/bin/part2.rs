ef3ae963ce67afad52cd6e3f12efa9c151119aa2
Roberto Mora <romogo17@gmail.com>
2023-12-16 09:09:48 -0600

    adds a daily template

---
+++ 2023/rust/daily-template/src/bin/part2.rs
@@ -0,0 +1,10 @@
+use {{crate_name}}::part2::process;
+use miette::Context;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    let input = include_str!("../../inputs/input1.txt");
+    let output = process(input).context("process part 2")?;
+    println!("Output is {output}");
+    Ok(())
+}
---
+++ 2023/rust/daily-template/src/part2.rs
@@ -0,0 +1,19 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(_input: &str) -> miette::Result<i64, AocError> {
+    todo!("{{project-name}} - part1");
+}
+
+#[cfg(test)]
+mod {{crate_name}}_part2 {
+    use super::*;
+
+    #[test]
+    fn example() -> miette::Result<()> {
+        todo!("haven't built test yet");
+        let input = "";
+        assert_eq!(100, process(input)?);
+        Ok(())
+    }
+}
