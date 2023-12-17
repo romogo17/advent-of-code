0eb57c5177734b8b56af92d7b183253e556b7cae
Roberto Mora <romogo17@gmail.com>
2023-12-16 22:18:24 -0600

    adds tracing to day-15-part1

--- 2023/rust/day-15/src/part1.rs
+++ 2023/rust/day-15/src/part1.rs
@@ -7,7 +7,7 @@ fn hash(input: &str) -> u8 {
         .fold(0u64, |acc, c| (acc + c as u64) * 17u64) as u8
 }
 
-#[tracing::instrument]
+#[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> miette::Result<u64, AocError> {
     let results: u64 = input.trim().split(",").map(|step| hash(step) as u64).sum();
     Ok(results)
