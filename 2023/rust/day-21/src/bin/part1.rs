cc64682f88addb1c00fc7ab7857f172459583aa3
Roberto Mora <romogo17@gmail.com>
2023-12-21 17:41:00 -0600

    adds day-21

---
+++ 2023/rust/day-21/src/bin/part1.rs
@@ -0,0 +1,18 @@
+use day_21::part1::process;
+
+use miette::Context;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer().compact())
+        .init();
+
+    let input = include_str!("../../inputs/input1.txt");
+    let output = process(input).context("process part 1")?;
+    println!("Output is {output}");
+    Ok(())
+}
---
+++ 2023/rust/day-21/src/part1.rs
@@ -0,0 +1,28 @@
+use crate::custom_error::AocError;
+use tracing::{debug, info};
+
+#[tracing::instrument(skip(input))]
+pub fn process(input: &str) -> miette::Result<u64, AocError> {
+    info!("running not implemented day_21_part1");
+    debug!(input, "with");
+    Ok(100)
+}
+
+#[cfg(test)]
+mod day_21_part1 {
+    use super::*;
+
+    #[test_log::test]
+    fn example() -> miette::Result<()> {
+        let input = "";
+        assert_eq!(100, process(input)?);
+        Ok(())
+    }
+
+    // #[test_log::test]
+    // fn input1() -> miette::Result<()> {
+    //     let input = include_str!("../inputs/input1.txt");
+    //     assert_eq!(100, process(input)?);
+    //     Ok(())
+    // }
+}
