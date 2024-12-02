fdbc2e19fc31177c1d35a3b32a280a77e1150a9d
Roberto Mora <romogo17@gmail.com>
2024-12-02 17:33:21 -0600

    add boilerplate for advent of code 2024

---
+++ 2024/rust/daily-template/src/bin/part2.rs
@@ -0,0 +1,18 @@
+use {{crate_name}}::part2::process;
+
+use miette::Context;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::EnvFilter;
+
+#[tracing::instrument]
+fn main() -> miette::Result<()> {
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer().compact())
+        .init();
+
+    let input = include_str!("../../inputs/input.txt");
+    let output = process(input).context("process part 2")?;
+    println!("Output is {output}");
+    Ok(())
+}
---
+++ 2024/rust/daily-template/src/part1.rs
@@ -0,0 +1,26 @@
+use crate::custom_error::AocError;
+use tracing::{debug, info};
+
+#[tracing::instrument(skip(input))]
+pub fn process(input: &str) -> miette::Result<u64, AocError> {
+    todo!("not implemented {{crate_name}}_part1");
+}
+
+#[cfg(test)]
+mod {{crate_name}}_part1 {
+    use super::*;
+
+    #[test_log::test]
+    fn example() -> miette::Result<()> {
+        let input = "";
+        assert_eq!("", process(input)?);
+        Ok(())
+    }
+
+    // #[test_log::test]
+    // fn input() -> miette::Result<()> {
+    //     let input = include_str!("../inputs/input.txt");
+    //     assert_eq!(100, process(input)?);
+    //     Ok(())
+    // }
+}
